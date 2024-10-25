69fe5a62eed07575beee62f330428165ef0a0b92
johndeniel2003 <128560485+johndeniel2003@users.noreply.github.com>
2024-10-25 22:44:41 +0800

    Add files via upload

---
+++ function.js/main.js
@@ -0,0 +1,32 @@
+import { parallaxIcons } from "./parallaxIcons.js";
+import { responsiveMenu } from "./responsiveMenu.js";
+import { scrollAnimation as progressBarAnimation } from "./scrollAnimation.js";
+import { textAnimation } from "./textAnimation.js";
+import { customCursor } from "./customCursor.js";
+import loading from "./loader.js";
+import { filter } from "./memberFilter.js";
+
+// FOR FILTERING MEMBER ANIMATION
+filter();
+
+// FOR LOADING SCREEN
+loading();
+
+//FOR MOVING ICONS
+parallaxIcons();
+
+//FOR RESPONSIVE MENU
+responsiveMenu();
+
+//FOR SCROLL ANIMATION
+progressBarAnimation();
+
+
+//FOR CUSTOM CURSOR
+customCursor();
+
+//FOR TEXT ANIMATION
+textAnimation();
+
+
+
